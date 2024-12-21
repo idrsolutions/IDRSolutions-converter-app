bb24c046ce1807fb6d85466f0b0d191cff929be0
Xinyu <Xinyu@Thes-MacBook-Pro.local>
2024-12-21 15:41:05 +0000

    modified formvu primary color

--- lib/themes/colors.dart
+++ lib/themes/colors.dart
@@ -6,6 +6,6 @@ class AppColors {
   static Color BuildVuSecondary = const Color.fromRGBO(203, 73, 22, 60);
   static Color JPedalPrimary = const Color.fromARGB(156, 0, 154, 171);
   static Color JPedalSecondary = const Color.fromRGBO(5, 127, 141, 60);
-  static Color FormVuPrimary = const Color.fromARGB(156, 238, 59, 122);
+  static Color FormVuPrimary = const Color.fromARGB(156, 228, 0, 80);
   static Color FormVuSecondary = const Color.fromARGB(156, 238, 59, 100);
 }
